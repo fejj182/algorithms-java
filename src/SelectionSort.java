35fa2f52f4fd4bff33d9b4eaa1d645d27cee3c08
Jeff Zinger <jzinger100@gmail.com>
2019-05-03 19:49:41 +0200

    SelectionSort refactor

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -4,17 +4,15 @@ public class SelectionSort {
     public void sort(List<Integer> list) {
         for (int i=0; i < list.size() - 1; i++) {
 
-            int min = list.get(i);
             int minIndex = i;
-            for (int j=i; j < list.size(); j++) {
-                if (list.get(j) < min) {
-                    min = list.get(j);
+            for (int j=i+1; j < list.size(); j++) {
+                if (list.get(j) < list.get(minIndex)) {
                     minIndex = j;
                 }
             }
 
             int temp = list.get(i);
-            list.set(i, min);
+            list.set(i, list.get(minIndex));
             list.set(minIndex, temp);
         }
     }
